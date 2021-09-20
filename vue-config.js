52a8c17d01f3402a8248bd4ae0d48959e43b1732
Antho <matvienko.anthony@gmail.com>
2021-09-20 16:47:37 +0200

    test

--- vue-config.js
+++ vue-config.js
@@ -2,9 +2,7 @@ module.exports = {
     pwa: {
         workboxPluginMode: 'InjectManifest',
         workboxOptions: {
-            // swSrc is required in InjectManifest mode.
-            swSrc: 'src/registerServiceWorker.js',
-            // ...other Workbox options...
-            exclude: ['_redirects'],
+            exclude: ['_redirects']
+        }
     }
 }
